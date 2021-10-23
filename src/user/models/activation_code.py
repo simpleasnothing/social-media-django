d2ed58b550baccb1d928398a0b6712c8764f9f96
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2021-10-23 23:31:42 +0330

    ActivationCode model

---
+++ src/user/models/activation_code.py
@@ -0,0 +1,47 @@
+
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import gettext as _
+
+from utils.general_model import GeneralModel
+
+
+User = get_user_model()
+
+
+class ActivationCode(GeneralModel):
+
+    CHOICES_TYPE = (
+        ("R", "Register"),
+        ("F", "Forgot"),
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=_("ActivationCode"),
+    )
+    type = models.CharField(
+        max_length=1,
+        choices=CHOICES_TYPE,
+        verbose_name=_("Type"),
+    )
+    code = models.CharField(
+        max_length=32,
+        verbose_name=_("Code"),
+    )
+    retry = models.PositiveSmallIntegerField(
+        verbose_name=_("Retry"),
+        default=0,
+    )
+    used = models.BooleanField(
+        default=False,
+        verbose_name=_("Used"),
+    )
+
+    class Meta:
+        verbose_name = _("ActivationCode")
+        verbose_name_plural = _("ActivationCodes")
+
+    def __str__(self):
+        return f"{self.user.username}"
