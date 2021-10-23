43b544d9746280d1f26108fed6e7bb1581943d03
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2021-10-23 22:31:57 +0330

    create SocialLink model

---
+++ src/user/models/social_link.py
@@ -0,0 +1,30 @@
+
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+User = get_user_model()
+
+
+class SocialLink(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name=_('User social media'),
+        on_delete=models.CASCADE,
+        related_name='social_links',
+    )
+    name = models.CharField(
+        verbose_name=_('Social media name'),
+        max_length=128,
+    )
+    url = models.URLField(
+        verbose_name=_('Social media url'),
+    )
+
+    class Meta:
+        verbose_name = _("SocialLink")
+        verbose_name_plural = _("SocialLinks")
+
+    def __str__(self):
+        return self.name
