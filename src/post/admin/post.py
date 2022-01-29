e7f1a519e129e80183c3cddaf2894a869c3ab30d
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2022-01-29 16:56:33 +0330

    modify admin pannel of the app: post

--- src/post/admin/post.py
+++ src/post/admin/post.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 
 class PostAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['caption']
+    list_display = ['id', 'caption', 'like_count']
+    list_filter = ['user', 'tag']
