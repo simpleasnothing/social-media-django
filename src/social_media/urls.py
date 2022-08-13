1c9d4644c18257c097d3ada76048fd6cafadc1c4
Ammar Salahi <ammarsaalahi@gmail.com>
2022-08-13 20:14:22 +0430

    Update urls.py

--- src/social_media/urls.py
+++ src/social_media/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 v1_urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('post/', include('post.api.urls', namespace='post')),
     path('user/', include('user.api.urls', namespace='user')),
 
