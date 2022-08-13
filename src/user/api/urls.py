51a7c4fd6f19fb079508e44fcbac04170c29c25d
Ammar Salahi <ammarsaalahi@gmail.com>
2022-08-13 20:59:52 +0430

    Update urls.py

--- src/user/api/urls.py
+++ src/user/api/urls.py
@@ -8,8 +8,7 @@ from user.api.views import UserLoginView, UserRegisterView
 
 app_name='user'
 urlpatterns=[
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+   
     path('login/',UserLoginView.as_view(),name='user-login'),
     path('register/',UserRegisterView.as_view(),name='user-register'),
 ]
