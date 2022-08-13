89cd7f75b497eadceed01cc6348ee4b3ac3563b7
ammarsalahi <ammarsaalahi@gmail.com>
2022-08-13 17:27:26 +0430

    Add User Register

--- src/user/api/views.py
+++ src/user/api/views.py
@@ -24,6 +24,7 @@ class UserRegisterView(generics.GenericAPIView):
 
     def post(self,request,*args,**kwargs):
         serializer=self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         user=serializer.save()
         token=RefreshToken.for_user(user)
         return Response({
